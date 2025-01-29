304f4c532d760ae5f3c254a76c87c06215c22d58
faizan-devstack <faizan.devstack@gmail.com>
2025-01-29 18:25:06 +0500

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,8 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#f02e65",
+        secondary: "fe9684",
       },
     },
   },
